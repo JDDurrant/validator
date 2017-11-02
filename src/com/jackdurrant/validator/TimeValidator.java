0a70df769dbcd222a3d102c71df5166a825b7a8b
Jack Durrant <jack@durrant.tech>
2017-11-02 11:39:04 +0000

    Worked on TimeValidator

--- src/com/jackdurrant/validator/TimeValidator.java
+++ src/com/jackdurrant/validator/TimeValidator.java
@@ -3,6 +3,7 @@ package com.jackdurrant.validator;
 import com.jackdurrant.libraries.Prompt;
 
 import java.text.ParseException;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -19,5 +20,25 @@ public class TimeValidator {
 
         System.out.println(time1);
         System.out.println(time2);
+
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.setTime(time1);
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.setTime(time2);
+
+        int hour1 = calendar1.HOUR;
+        int minute1 = calendar1.MINUTE;
+        int hour2 = calendar2.HOUR;
+        int minute2 = calendar2.MINUTE;
+
+        System.out.println(hour1);
+        System.out.println(hour2);
+        System.out.println(minute1);
+        System.out.println(minute2);
+
+        int hourTotal = hour1 + hour2;
+        int minuteTotal = minute1 + minute2;
+
+        System.out.printf("Total: %d:%d", hourTotal, minuteTotal);
     }
 }
